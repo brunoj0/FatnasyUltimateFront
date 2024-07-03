d2be0d1f9153b7b7c8c238d2e44a8a01586420b0
brunoj <bruno.jamula@gmail.com>
2024-07-03 15:28:57 +0200

    test

--- src/app/players-list/players-list.component.ts
+++ src/app/players-list/players-list.component.ts
@@ -7,7 +7,10 @@ import { Player } from '../core/models/player.model';
 import { map } from 'rxjs/internal/operators/map';
 import { startWith } from 'rxjs/internal/operators/startWith';
 import { LeagueStore } from '../core/league.store';
-import { addPlayerAvailability, playerFullNameToShortVersion } from '../shared/utls';
+import {
+  addPlayerAvailability,
+  playerFullNameToShortVersion,
+} from '../shared/utls';
 import { CommonModule } from '@angular/common';
 import { shareReplay, switchMap, tap } from 'rxjs';
 import { toObservable } from '@angular/core/rxjs-interop';
@@ -16,29 +19,40 @@ import { MyTeamPlayer } from '../my-team/my-team-player.model';
 @Component({
   selector: 'app-players-list',
   standalone: true,
-  imports: [GenericGridComponentComponent, MatInputModule, FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [
+    GenericGridComponentComponent,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './players-list.component.html',
-  styleUrl: './players-list.component.scss'
+  styleUrl: './players-list.component.scss',
 })
 export class PlayersListComponent {
-  readonly displayedColumns = ['availability','name', 'assists', 'points'];
+  readonly displayedColumns = ['availability', 'name', 'assists', 'points'];
 
-  playerSearch = new FormControl('', {nonNullable: true});
+  playerSearch = new FormControl('', { nonNullable: true });
   readonly store = inject(LeagueStore);
 
   players = toObservable(this.store.playerWithAvailability).pipe(
-    switchMap(players => this.playerSearch.valueChanges.pipe(
-      map((searchTerm: string) => {
-        if (searchTerm === '') {
-          return players;
-        }
-        return players.filter((player: any) => playerFullNameToShortVersion(player).name.toLowerCase().includes(searchTerm.toLowerCase()));
-      }),
-      startWith(players),
-  map((players: Player[]) => players.map(playerFullNameToShortVersion)),
-      tap(data => console.log(data)),
-      // shareReplay(1)
+    switchMap((players) =>
+      this.playerSearch.valueChanges.pipe(
+        map((searchTerm: string) => {
+          if (searchTerm === '') {
+            return players;
+          }
+          return players.filter((player: any) =>
+            playerFullNameToShortVersion(player)
+              .name.toLowerCase()
+              .includes(searchTerm.toLowerCase())
+          );
+        }),
+        startWith(players),
+        map((players: Player[]) => players.map(playerFullNameToShortVersion)),
+        tap((data) => console.log(data))
+        // shareReplay(1)
+      )
     )
-  )
   );
 }
